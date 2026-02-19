caf20d20f06cfbcc292b6cd70c061469923e1961
kapil <kapilgoriya3@gmail.com>
2026-02-19 18:11:07 +0530

    Fix navbar links

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,12 +1,14 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
     <div className="navbar">
       <div className="nav-title">S-Courier System</div>
 
       <div className="nav-links">
-        <a href="/">Add Entry</a>
-        <a href="/records">Records</a>
-        <a href="/billing">Billing</a>
+        <Link to="/">Add Entry</Link>
+        <Link to="/records">Records</Link>
+        <Link to="/billing">Billing</Link>
       </div>
     </div>
   );
