df1b53ad32b4c691ad38a20272cd1dd34b1aaa95
kapil <kapilgoriya3@gmail.com>
2026-02-23 12:09:07 +0530

    Added admin login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,17 @@ import AddEntry from "./pages/AddEntry";
 import Records from "./pages/Records";
 import Billing from "./pages/Billing";
 import { HashRouter  } from "react-router-dom";
-
+import Login from "./pages/Login";
 
 function App() {
+  const token = localStorage.getItem("token");
+
+if (!token) {
+  return <Login />;
+}
   return (
+
+
     <HashRouter>
       <Navbar/>
       <Routes>
---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function Login() {
+  const [form, setForm] = useState({ username: "", password: "" });
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleLogin = async () => {
+    const res = await fetch(
+      "https://s-courier-system.onrender.com/api/auth/login",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(form)
+      }
+    );
+
+    const data = await res.json();
+
+    if (res.ok) {
+      localStorage.setItem("token", data.token);
+      navigate("/");
+    } else {
+      alert(data.error);
+    }
+  };
+
+  return (
+   <div className="login-page">
+  <div className="login-card">
+    <h2>Admin Login</h2>
+
+    <input
+      name="username"
+      placeholder="Username"
+      onChange={handleChange}
+    />
+
+    <input
+      name="password"
+      type="password"
+      placeholder="Password"
+      onChange={handleChange}
+    />
+
+    <button onClick={handleLogin}>Login</button>
+  </div>
+</div>
+  );
+}
+
+export default Login;
